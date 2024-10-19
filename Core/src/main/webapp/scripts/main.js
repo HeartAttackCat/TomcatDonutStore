349a84260b9f8e1e6eacb513d87cfd5d75612c5b
HeartAttackCat <hunter.lane@student.nmt.edu>
2024-10-18 22:48:10 -0600

    getting ready to connect front to back.

--- Core/src/main/webapp/scripts/main.js
+++ Core/src/main/webapp/scripts/main.js
@@ -1,3 +1,16 @@
 
+function getMenu() {
+	const url = "./api/menu";
+    fetch(url).then(
+        function accept(response) {
+            var menujson = response.json();
+        },
+        function reject(reason) {
+			console.log("Error server did not respond.")
+        }
+    )
+}
+
 
 console.log("Hello world");
+getMenu();
