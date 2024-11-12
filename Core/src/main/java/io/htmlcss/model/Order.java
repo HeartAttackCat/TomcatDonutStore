682226f0ece5cde87fca1cbb23060421799b4fab
MoonBasedKas <kassidy.maberry@gmail.com>
2024-11-11 20:38:22 -0700

    Reverting order from when I accidently copied customer into it

--- Core/src/main/java/io/htmlcss/model/Order.java
+++ Core/src/main/java/io/htmlcss/model/Order.java
@@ -1,64 +1,28 @@
 /**
- * A container for customer information.
+ * A basic container for information related to a singular item being ordered.
  */
 package io.htmlcss.model;
 
-public class Customer {
-    
-    private String firstName = "";
-    private String lastName = "";
-    private String address = "";
-    private String phoneNumber = "";
-    private String email = "";
-    private int cardID = 0; // card used to purchase. Int is probably the wrong data type lol
-    private int zipCode = 0;
+public class Order {
+    private Donut item;
+    private int quantity;
 
     /**
-     * Basic constructor for class.
-     * @param firstName
-     * @param lastName
-     * @param address
-     * @param phoneNumber
-     * @param email
-     * @param cardID
-     * @param zipCode
+     * Constructor
+     * @param item
+     * @param quantity
      */
-    public Customer(String firstName, String lastName, String address, String phoneNumber, String email, int cardID, int zipCode){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.cardID = cardID;
-        this.zipCode = zipCode;
-
+    public Order(Donut item, int quantity){
+        this.item = item;
+        this.quantity = quantity;
     }
 
-    public String getFirstName(){
-        return this.firstName;
-    }
-    
-    public String getLastName(){
-        return this.lastName;
+
+    public Donut getItem(){
+        return this.item;
     }
 
-    public String getAddress(){
-        return this.address;
-    }
-
-    public String getPhoneNumber(){
-        return this.phoneNumber;
-    }
-
-    public String getEmail(){
-        return this.email;
-    }
-
-    public int getZipCode(){
-        return this.zipCode;
-    }
-    
-    public int getCardID(){
-        return this.cardID;
+    public int getQuantity(){
+        return this.quantity;
     }
 }
