0973b4d331856d77acde975b935bd3d18d54f06e
Hunter Lane <hunterlane@Hunters-Mac-mini.local>
2024-11-13 19:07:38 -0700

    Finished implementing the cart servlet.

--- Core/src/main/java/io/htmlcss/api/CartDispatcher.java
+++ Core/src/main/java/io/htmlcss/api/CartDispatcher.java
@@ -1,8 +1,10 @@
 package io.htmlcss.api;
 
 import java.io.IOException;
+import java.util.List;
 
 import io.htmlcss.model.Cart;
+import io.htmlcss.model.Order;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -43,7 +45,7 @@ public class CartDispatcher extends HttpServlet {
 				addToCart(request, response, cart);
 			}
 			else if (command.equalsIgnoreCase("remove")) {
-				
+				removeFromCart(request, response, cart);
 			}
 			else if (command.equalsIgnoreCase("show")) {
 				showCart(request, response, cart);
@@ -60,28 +62,77 @@ public class CartDispatcher extends HttpServlet {
 			return;
 		}
 		
-		request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
+		//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-REMOVE BLOCK WHEN CART.JSP IS DONE-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+		List<Order> orders = cart.getItems();
+		
+		for(Order order: orders) {
+			response.getWriter().append("[Cart Item] " + order.getItem().getType() + " " + order.getItem().getFlavor() + " | amount "+order.getQuantity()+", Subtotal: "+order.getQuantity()*order.getItem().getPrice()+"\n");
+		}
+		
+		response.getWriter().append("[Cart Total] " + cart.getTotalCost() + "\n");
+		//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+		
+		
+		//TODO uncomment next line when cart.jsp is needed
+		// request.getRequestDispatcher("/WEB-INF/cart.jsp").forward(request, response);
 	}
 	
 	public void addToCart(HttpServletRequest request, HttpServletResponse response, Cart cart) throws IOException, ServletException {
-		if (cart == null)
-		{
-			showCart(request, response, cart);
+		String donutIDString = request.getParameter("did");
+		String donutQuanString = request.getParameter("quantity");
+		
+		if (donutIDString == null || donutQuanString == null) {
+			request.getRequestDispatcher("/WEB-INF/fof.jsp").forward(request, response);
 			return;
 		}
 		
-		String donutID = request.getParameter("did");
-		String donutQuan = request.getParameter("quan");
+		if (cart == null) {
+			Cart newCart = new Cart();
+			cart = newCart;
+		}
 		
+		int donutID = Integer.parseInt(donutIDString);
+		int donutQuan = Integer.parseInt(donutQuanString); 
 		
+		if (!cart.addDonuts(donutID, donutQuan))
+		{
+			request.getRequestDispatcher("/WEB-INF/fof.jsp").forward(request, response);
+			return;
+		}
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("cart", cart);
+		
+		showCart(request, response, cart);
 	}
 	
 	public void removeFromCart(HttpServletRequest request, HttpServletResponse response, Cart cart) throws IOException, ServletException {
-		if (cart == null)
-		{
-			showCart(request, response, cart);
+		String donutIDString = request.getParameter("did");
+		String donutQuanString = request.getParameter("quantity");
+		
+		if (donutIDString == null || donutQuanString == null) {
+			request.getRequestDispatcher("/WEB-INF/fof.jsp").forward(request, response);
 			return;
 		}
+		
+		if (cart == null) {
+			Cart newCart = new Cart();
+			cart = newCart;
+		}
+		
+		int donutID = Integer.parseInt(donutIDString);
+		int donutQuan = Integer.parseInt(donutQuanString); 
+		
+		if (!cart.removeDonuts(donutID, donutQuan))
+		{
+			request.getRequestDispatcher("/WEB-INF/fof.jsp").forward(request, response);
+			return;
+		}
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("cart", cart);
+		
+		showCart(request, response, cart);
 	}
 	
 	/**
