9abdf7572542c9fd680339de85a02517eee27edf
Kor <korbin.shelley@student.nmt.edu>
2024-11-14 23:11:02 -0700

    Update sales report

--- Core/src/main/java/io/htmlcss/model/ReportData.java
+++ Core/src/main/java/io/htmlcss/model/ReportData.java
@@ -3,6 +3,8 @@ import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
+import io.htmlcss.db.DBFactory;
+
 public class ReportData {
 	// Dictionaries which will contain our item information.
 	private Dictionary<Integer, Integer> dQuant = null; // id -> quantity
@@ -28,7 +30,9 @@ public class ReportData {
 		}
 	}
 	
-	
+	public Dictionary<Integer, Integer> getdQuant() {
+		return this.dQuant;
+	}
 	
 	/**
 	 * Obtains the total amount of items that have been sold.
@@ -60,5 +64,31 @@ public class ReportData {
 	
 	public Enumeration<Integer> getDonutIds() {
 		return this.dQuant.keys();
+	}
+
+	public static ReportData getSalesReport(String date, int range) {
+		// Verify that the date is in the correct format.
+		if (!date.matches("\\d{4}-\\d{2}-\\d{2}")) {
+			throw new IllegalArgumentException("Invalid date format. Expected format: yyyy-MM-dd");
+		}
+		// Verify that the range is a positive integer.
+		if (range < 0) {
+			throw new IllegalArgumentException("Invalid range. Expected a positive integer.");
+		}
+		
+		return DBFactory.getDatabaseFetcher().generateSalesReport(date, range);
+	}
+
+	public static ReportData getStaleReport(String date, int range) {
+		// Verify that the date is in the correct format.
+		if (!date.matches("\\d{4}-\\d{2}-\\d{2}")) {
+			throw new IllegalArgumentException("Invalid date format. Expected format: yyyy-MM-dd");
+		}
+		// Verify that the range is a positive integer.
+		if (range < 0) {
+			throw new IllegalArgumentException("Invalid range. Expected a positive integer.");
+		}
+		
+		return DBFactory.getDatabaseFetcher().generateStaleReport(date, range);
 	}
 }
